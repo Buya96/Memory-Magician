cbd573efd76407a5ee3f6df7b0c725360ee8de2c
Buya96 <ayub1231@hotmail.com>
2025-08-08 13:49:53 +0100

    added date and game state

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ const chemistryPairs = [
     { id: 7, element: "Gold", symbol: "Au" },
     { id: 8, element: "Silver", symbol: "Ag" }
 ];
+
+// Game state
+let gameBoard = [];
+let flippedCards = [];
+let matchedPairs = 0;
+let moves = 0;
+let gameStarted = false;
+let gameTimer = 0;
+let timerInterval = null;
